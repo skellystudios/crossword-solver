8a9213e36a242e6288cd099d1c1a936380eae57b
Michael Skelly <skellystudios@gmail.com>
2014-05-01 22:55:39 +0100

    Ah yeah baby solve dem clues

--- coding/solver.hs
+++ coding/solver.hs
@@ -1,9 +1,14 @@
 module Solver where 
 
-data ClueTree = DefNode String ClueTree | ConsNode ClueTree ClueTree | Leaf String | AnagramNode Anagrind String 
- deriving Show
+import Data.List
 
-data Anagrind = Indicator String
+data Clue = DefNode String ClueTree 
+  deriving Show
+
+data ClueTree = ConsNode ClueTree ClueTree | Leaf String | AnagramNode Anagrind [String]
+  deriving Show
+
+data Anagrind = Indicator [String]
  deriving Show
 
 includeReversals xs = xs ++ [(snd(x),fst(x)) | x <- xs] 
@@ -20,7 +25,7 @@ partitions (x:xs) = [[x]:p | p <- partitions xs] ++ [(x:ys):yss | (ys:yss) <- pa
 
 makeTree = (makeDefs . words) 
 
-makeDefs :: [String] -> [ClueTree]
+makeDefs :: [String] -> [Clue]
 makeDefs xs = let parts = twoParts xs
 			  in concat [[DefNode (concatWithSpaces (fst part)) y' | y' <- (expand (snd part))] | part <- includeReversals (parts)]
 
@@ -34,10 +39,11 @@ makeConsNodes :: [String] -> [ClueTree]
 makeConsNodes xs = let parts = twoParts xs
                    in concat [[ConsNode x' y' |x' <- (expand (fst part)), y' <- (expand (snd part))] | part <- parts]  
 
+
 -- Sometimes need to use synonymns here
 makeAnagramNodes :: [String] -> [ClueTree]
 makeAnagramNodes xs = let parts = twoParts xs
-                   in concat [[AnagramNode (Indicator x') y' | x' <- parts, y' <- parts, x' == (fst part), y' == (snd part), isAnagramWord x'] | part <- parts]  
+                  in [AnagramNode (Indicator x) y | (x,y) <- parts, isAnagramWord(x)] 
 
 item2 x = head (tail x)
 
@@ -49,4 +55,29 @@ isAnagramWord _ = False
 concatWithSpaces (x:[]) = x
 concatWithSpaces (x:xs) = x ++ " " ++ concatWithSpaces xs
  
-clue1 = words "Companion shredded corset"
+clue1 = words "companion shredded corset"
+
+-- Now we evaluate
+eval :: Clue -> [String]
+eval x = let DefNode y z = x in Data.List.intersect (syn y) (eval_tree z)
+
+eval_tree :: ClueTree -> [String]
+eval_tree (AnagramNode x y) = anagrams(concat(y))
+eval_tree (Leaf x) = syn x
+eval_tree (ConsNode x y) = [x' ++ y' | x' <- eval_tree(x), y' <- eval_tree(y)]
+
+syn :: String -> [String]
+syn "companion" = ["friend", "escort"]
+syn "shredded" = ["changed", "stripped"]
+syn "corset" = ["basque"]
+syn _ = []
+
+anagrams :: String -> [String]
+anagrams x = anagrams1 x []
+
+anagrams1 :: String -> String -> [String]
+anagrams1 [] ys = [ys]
+--anagrams1 (x:[]) ys = [x:ys, ys ++ [x]]
+anagrams1 (x:xs) ys = (anagrams1 xs (x:ys)) ++ (anagrams1 xs (ys++[x]))
+
+
