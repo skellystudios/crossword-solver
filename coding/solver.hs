25afa48e7f24557d3a664af83138f7f8fac30368
Michael Skelly <skellystudios@gmail.com>
2014-05-02 23:49:07 +0100

    Oh yeah baby, insertions as well

--- coding/solver.hs
+++ coding/solver.hs
@@ -5,15 +5,17 @@ import Data.List
 data Clue = DefNode String ClueTree 
   deriving Show
 
-data ClueTree = ConsNode ClueTree ClueTree | Leaf String | AnagramNode Anagrind [String]
+data ClueTree = ConsNode ClueTree ClueTree | Leaf String | AnagramNode Anagrind [String] | InsertionNode InsertionIndicator ClueTree ClueTree
   deriving Show
 
-data Anagrind = Indicator [String]
- deriving Show
+data Anagrind = AIndicator [String] deriving Show
+data InsertionIndicator = IIndicator [String] deriving Show
+
 
 includeReversals xs = xs ++ [(snd(x),fst(x)) | x <- xs] 
 
 twoParts xs = map (\x -> (head x, (head . tail) x)) (nPartitions 2 xs)
+threeParts xs = map (\x -> (head x, (head . tail) x , (head . tail . tail) x)) (nPartitions 3 xs)
 
 nPartitions :: Int -> ([String] -> [[[String]]])
 nPartitions n xs = [xss | xss <- partitions xs, length xss == n]
@@ -33,6 +35,7 @@ expand :: [String] -> [ClueTree]
 expand ys = [Leaf (concatWithSpaces ys)] 
 	++ (if length ys > 1 then makeAnagramNodes ys else [] )
 	++ (if length ys > 1 then makeConsNodes ys else [])
+	++ (if length ys > 2 then makeInsertionNodes ys else [])
 
 
 makeConsNodes :: [String] -> [ClueTree]
@@ -40,37 +43,19 @@ makeConsNodes xs = let parts = twoParts xs
                    in concat [[ConsNode x' y' |x' <- (expand (fst part)), y' <- (expand (snd part))] | part <- parts]  
 
 
--- Sometimes need to use synonymns here
+
+-- ANAGRAMS
+ 
+
+-- Sometimes need to use synonymns here ??? Maybe anagram subtypes needs to be a special type of subtree
 makeAnagramNodes :: [String] -> [ClueTree]
 makeAnagramNodes xs = let parts = twoParts xs
-                  in [AnagramNode (Indicator x) y | (x,y) <- parts, isAnagramWord(x)] 
-
-item2 x = head (tail x)
+                  in [AnagramNode (AIndicator x) y | (x,y) <- parts, isAnagramWord(x)] 
 
 isAnagramWord :: [String] -> Bool
 isAnagramWord ["mixed"] = True
 isAnagramWord ["shredded"] = True
 isAnagramWord _ = False
-
-concatWithSpaces (x:[]) = x
-concatWithSpaces (x:xs) = x ++ " " ++ concatWithSpaces xs
- 
-clue1 = words "companion shredded corset"
-
--- Now we evaluate
-eval :: Clue -> [String]
-eval x = let DefNode y z = x in Data.List.intersect (syn y) (eval_tree z)
-
-eval_tree :: ClueTree -> [String]
-eval_tree (AnagramNode x y) = anagrams(concat(y))
-eval_tree (Leaf x) = syn x
-eval_tree (ConsNode x y) = [x' ++ y' | x' <- eval_tree(x), y' <- eval_tree(y)]
-
-syn :: String -> [String]
-syn "companion" = ["friend", "escort"]
-syn "shredded" = ["changed", "stripped"]
-syn "corset" = ["basque"]
-syn _ = []
 
 anagrams :: String -> [String]
 anagrams x = anagrams1 x []
@@ -81,3 +66,50 @@ anagrams1 [] ys = [ys]
 anagrams1 (x:xs) ys = (anagrams1 xs (x:ys)) ++ (anagrams1 xs (ys++[x]))
 
 
+-- INSERTIONS
+
+-- Sometimes need to use synonymns here ??? Maybe anagram subtypes needs to be a special type of subtree
+makeInsertionNodes :: [String] -> [ClueTree]
+makeInsertionNodes xs = let parts = threeParts xs
+                  in [InsertionNode (IIndicator y) x' z' | (x,y,z) <- parts, isInsertionWord(y), x' <- (expand x), z' <- (expand z)] 
+
+
+insertInto :: String -> String -> [String] -- TODO: we don't want to have insertInto 'abc' 'xyz' = abcxyz
+insertInto xs [] = [xs]
+insertInto xs (y:ys) = [y:(xs ++ ys)] ++ (map ((:) y) (insertInto xs ys)) 
+
+
+isInsertionWord ["in"] = True
+isInsertionWord _ = False
+
+
+concatWithSpaces (x:[]) = x
+concatWithSpaces (x:xs) = x ++ " " ++ concatWithSpaces xs
+ 
+clue1 = words "companion shredded corset"
+clue2 = words "notice in flying coat notice in flying coat"
+
+-- Now we evaluate
+eval :: Clue -> [String]
+eval x = let DefNode y z = x in Data.List.intersect (syn y) (eval_tree z)
+
+eval_tree :: ClueTree -> [String]
+eval_tree (AnagramNode x y) = anagrams(concat(y))
+eval_tree (Leaf x) = syn x ++ [x]
+eval_tree (ConsNode x y) = [x' ++ y' | x' <- eval_tree(x), y' <- eval_tree(y)]
+eval_tree (InsertionNode ind x y) = concat[insertInto x' y' | x' <- eval_tree(x), y' <- eval_tree(y)]
+
+syn :: String -> [String]
+syn "notice" = ["ack", "acknowledge", "sign"] ++  [show x | x <- [1..50]]
+syn "coat" = ["jacket"]
+syn "companion" = ["friend", "escort"]
+syn "shredded" = ["changed", "stripped"]
+syn "corset" = ["basque"]  ++  [show x | x <- [1..50]]
+syn "flying" = ["jet"]  ++  [show x | x <- [1..50]]
+syn _ = []
+
+
+
+
+-- Cons node equivalence - write it as a list, don't allow cons node as a child
+-- Memoiszation
