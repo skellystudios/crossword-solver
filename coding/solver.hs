dcb48a5636e9335847d1762455b6d059b98b6a38
Michael Skelly <skellystudios@gmail.com>
2014-05-20 23:44:16 +0100

    Printing and fun and stuff

--- coding/solver.hs
+++ coding/solver.hs
@@ -50,16 +50,25 @@ fastest_f = index f_tree
 --- DISPLAY FUNCTIONS
 
 showDef :: Clue -> String
-showDef (DefNode d tree n) = "Definition: " ++ show d ++ " \n" ++ showTree tree 1 ++ " \n" 
+showDef (DefNode d tree n) = "Definition: " ++ show d ++ " \n" ++ showTree tree 1 ++ " \n\n" 
 
 showTree :: ClueTree -> Int -> String
-showTree (ConsNode x y) n = spaces n ++ showTree x (n+1) ++ " \n" ++ showTree y (n+1)
+showTree (ConsNode x y) n = spaces n ++ showTreeL x (n+1) ++ showTreeL y (n+1)
+showTree (ConsListNode xs) n = spaces n ++ "Cons \n" ++ concat (map (\x -> (showTreeL x (n+1))) xs)
 showTree (AnagramNode (AIndicator anagrinds) strings) n = spaces n ++ "Anagram (" ++ concatWithSpaces anagrinds ++ ") " ++ concat strings
-showTree x n = spaces n ++ show x
+showTree (InsertionNode (IIndicator ind) t1 t2) n = spaces n ++ "Insert ("++ concatWithSpaces ind++") \n" ++ showTreeL t1 (n+1) ++ spaces n ++ "into" ++ " \n" ++ showTreeL t2 (n+1)
+showTree x n = spaces n ++ show x 
 
 
 spaces 0 = ""
 spaces n = "    " ++ spaces (n-1)
+
+showTreeL x n = showTree x n ++ "\n"
+
+print_this def = (putStr . showDef) def
+-- why can't we do map putstr
+print_all defs = mapM print_this defs
+-- ah that's why
 
 
 {- 
