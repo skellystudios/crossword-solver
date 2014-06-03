188f51a9020a88e21cb807c6ccf671df57cdc128
Michael Skelly <skellystudios@gmail.com>
2014-06-03 07:41:10 +0100

    Constraints during eval, much win

--- coding/solver.hs
+++ coding/solver.hs
@@ -11,7 +11,7 @@ import Wordlist
 data Parse = DefNode String ClueTree Int
   deriving Show
 
-data ClueTree = ConsListNode [ClueTree] | ConsNode ClueTree ClueTree | Leaf String | AnagramNode Anagrind [String] | InsertionNode InsertionIndicator ClueTree ClueTree | SubtractionNode SubtractionIndicator ClueTree ClueTree | HiddenWordNode HWIndicator [String] | ReversalNode ReversalIndicator ClueTree
+data ClueTree = ConsListNode [ClueTree] | ConsNode ClueTree ClueTree | Leaf String | AnagramNode Anagrind [String] | InsertionNode InsertionIndicator ClueTree ClueTree | SubtractionNode SubtractionIndicator ClueTree ClueTree | HiddenWordNode HWIndicator [String] | ReversalNode ReversalIndicator ClueTree | FirstLetterNode FLIndicator [String]
   deriving Show
 
 data Answer = Answer String Parse deriving Show
@@ -21,6 +21,7 @@ data InsertionIndicator = IIndicator [String] deriving Show
 data SubtractionIndicator = SIndicator [String] deriving Show
 data ReversalIndicator = RIndicator [String] deriving Show
 data HWIndicator = HWIndicator [String] deriving Show
+data FLIndicator = FLIndicator [String] deriving Show
 
 data Constrains = MaxLength MinLength
 data MaxLength = D Int
@@ -29,17 +30,17 @@ data MinLength = Int
 
 --- TOOD SECTION
 -- TODO: Preprocessing to weight to most likely: first (map c) will be lazy
--- TODO: Pass constraints down the eval chain, in the same way we pass them down the parse chain
+
 -- TODO: A function that makes a printed clue markup version (Clue -> String) [ah, but hard, as we don't create a total parse tree including subs etc, at the end]
+-- TODO: Conditional eval on insertion node should be smarter 
 
 -- TODO: do a thing wherein we deal with the problem with leaf nodes not evaluating to anything. THIS IS WHERE I CAN USE A MAYBE A MONAD
--- TODO: Sometimes need to use synonymns when doing anagrams ??? Maybe anagram subtypes needs to be a special type of subtree
 -- TODO: we don't want to have insertInto 'abc' 'xyz' = abcxyz
 -- TODO: Change subtraction eval function from insert, obvs
 -- TODO: Add some abbreviation function
--- TODO: Reversal clues
 -- TODO: First/last letter clues
 -- TODO: Before/after clues
+-- TODO: Sometimes need to use synonymns when doing anagrams ??? Maybe anagram subtypes needs to be a special type of subtree
 
 --- DISPLAY FUNCTIONS
 
@@ -132,6 +133,7 @@ minLength (InsertionNode ind tree1 tree2) = (minLength tree1) + (minLength tree2
 minLength (SubtractionNode ind tree1 tree2) = maximum[(minLength tree1) - (maxLength tree2),3]
 minLength (ReversalNode ind tree) = minLength tree
 minLength (Leaf string) = let x = minimum ( map length (string : syn string)) in x
+minLength (FirstLetterNode ind strings) = length strings
 
 
 maxLength (ConsListNode trees) = (sum . map maxLength) trees
@@ -141,7 +143,7 @@ maxLength (InsertionNode ind tree1 tree2) = (minLength tree1) + (minLength tree2
 maxLength (SubtractionNode ind tree1 tree2) = minimum[(maxLength tree1) - (minLength tree2),3]
 maxLength (ReversalNode ind tree) = maxLength tree
 maxLength (Leaf string) = let x = maximum ( map length (string : syn string)) in x
-
+maxLength (FirstLetterNode ind strings) = length strings
 
 ---------------- CLUE TYPES ----------------
 
@@ -232,7 +234,7 @@ isRIndicator _ = False
 -- HIDDEN WORDS
 makeHiddenWordNodes :: [String]  -> Int -> [ClueTree]
 makeHiddenWordNodes xs n = let parts = twoParts xs
-                  in [HiddenWordNode (HWIndicator x) y | (x,y) <- parts, isHWIndicator(x)] 
+                  in [HiddenWordNode (HWIndicator x) y | (x,y) <- parts, isHWIndicator(x)] --, (length (concat y)) > n 
 
 isHWIndicator ["found","in"] = True
 isHWIndicator ["needed","by"] = True
@@ -244,6 +246,17 @@ substr (x:xs) = (map ((:) x) (contiguoussubstr xs)) ++ substr xs
 contiguoussubstr [] = [[]]
 contiguoussubstr (x:xs) = [[x]] ++ (map ((:) x) (contiguoussubstr xs))
 
+
+
+-- FIRST LETTERS
+makeFirstLetterNodes :: [String]  -> Int -> [ClueTree]
+makeFirstLetterNodes xs n = let parts = twoParts xs
+                  in [FirstLetterNode (FLIndicator x) y | (x,y) <- includeReversals(parts), isFLIndicator(x), (length y) <= n]
+
+firstLetter = map head
+
+isFLIndicator ["at", "first"] = True
+isFLIndicator _ = False
 
 
 --------------------------- EVALUATION ----------------------------
@@ -282,11 +295,11 @@ eval (DefNode y z n) = let constraints = (n, n) in [Answer x (DefNode y z n) | x
 eval_tree :: Int -> ClueTree  -> [String]
 eval_tree n (AnagramNode x y) = if length(concat(y)) > n then [] else anagrams(concat(y))
 eval_tree n (Leaf x) = filter (\x -> length x <= n) (syn x ++ [x])
-eval_tree n (ConsListNode xs) = map concat (sequence (map (eval_tree n) xs))
+eval_tree n (ConsListNode xs) = eval_trees n xs --map concat (sequence (map (eval_tree n) xs))
 eval_tree n (ConsNode x y) = [x' ++ y' | x' <- eval_tree n x, y' <- eval_tree n y]
-eval_tree n (InsertionNode ind x y) = concat[insertInto x' y' | x' <- eval_tree n x, y' <- eval_tree n y]
+eval_tree n (InsertionNode ind x y) = concat[insertInto x' y' | x' <- eval_tree n x, y' <- eval_tree n y, (length x') + (length y') <= n]
 eval_tree n (SubtractionNode ind x y) = concat[subtractFrom x' y' | x' <- eval_tree n x, y' <- eval_tree n y]
-eval_tree n (HiddenWordNode ind ys) = [x | x <- substr (concat ys), (length x) > 0]
+eval_tree n (HiddenWordNode ind ys) = [x | x <- substr (concat ys), (length x) > 0, (length x) <= n]
 eval_tree n (ReversalNode ind ys) = map reverse (eval_tree n ys)
 
 eval_trees :: Int -> [ClueTree] -> [String]
@@ -307,8 +320,9 @@ find_solutions xs = map (\x -> (x, eval x)) xs
 -- solve = ignore_blanks . (map eval) . parse
 -- solve c =  map (check_eval) (parse c)
 
-solve = (check_synonyms . check_valid_words . constrain_lengths  . evaluate . parse . clue)
+solve = (check_synonyms . check_valid_words . constrain_lengths  . evaluate . parse)
 
+solve_clue = (solve . clue)
 --------------------------- DICTIONARY CORNER ----------------------------
 
 
@@ -316,8 +330,7 @@ wordlist_extended = Data.Set.union (Data.Set.fromList ["swanlake", "angela", "tu
 
 
 syn :: String -> [String]
-
--- {-
+ {-
 
 syn "notice" = ["ack", "acknowledge", "sign"] 
 syn "coat" = ["jacket"]
@@ -337,7 +350,8 @@ syn "put food in this" = ["tuckerbag"]
 
 -- -}
 
- {-
+
+-- {-
 
 syn "notice" = ["ack", "account","acquaintance","admonition","advertisement","advice","advisory","alarm","analysis","announcement","annunciation","appreciation","assiduity","attend","attention","awareness","behold","blackmail","blue book","briefing","bulletin","call for","call","care","caution","caveat","censure","circular","claim","cognizance","comment","commentary","communication","communique","conceive","concentration","consciousness","consideration","contribution","credible","criticism","critique","data","datum","declaration","descry","detect","diligence","directive","directory","discern","discharge","discover","dismissal","dispatch","distinguish","dope","draft","drain","draught","draughtsman","draughty","duty","ear","earnestness","edict","editorial","encyclical","enlightenment","enunciation","espial","espionage","espy","evidence","exaction","example","extortion","facts","feel","find","glimpse","gloss","goods","guidebook","handout","hark","heed","hint","identify","imposition","impost","indent","info","injunction","insight","inspect","instruction","intelligence","intentness","interdict","interest","item","ken","know","knowledge","leader","lesson","levy","light","look on","look","lookout","make out","mandate","manifesto","mark","memo","memorandum","mention","message","mind","monition","moral","note","notice","notice","account","acknowledge","acquaintance","admonish","admonishment","admonition","advert","advertence","advertency","advice","advise","alarm","alertness","allude","analysis","animadvert","announce","announcement","annunciation","apperception","appreciation","appreciativeness","apprehension","approval","assiduity","assiduousness","attend","attend to","attention","attention span","attentiveness","awareness","behold","bench warrant","blackmail","blue book","book review","briefing","bulletin","bulletin board","call","call for","capias","care","catch sight of","caution","caveat","censure","circular","claim","clap eyes on","cognition","cognizance","comment","commentary","commentation","communication","communique","concentration","concern","consciousness","consideration","contribution","critical bibliography","critical journal","critical notice","critical review","criticism","critique","data","datum","death warrant","declaration","demand","demand for","descry","detect","deterrent example","diligence","directive","directory","discern","discover","dispatch","distinguish","draft","drain","duty","ear","earnestness","edict","editorial","encyclical","enlightenment","enunciation","espial","espionage","espy","evidence","exaction","example","extortion","extortionate demand","facts","factual information","familiarization","fieri facias","final notice","final warning","gen","general information","give heed to","give notice","glimpse","gloss","grasp","guidebook","habere facias possessionem","handout","hard information","have in sight","heavy demand","heed","heedfulness","hint","identify","imposition","impost","incidental information","indent","info","inform","information","injunction","insight","insistent demand","instruction","intelligence","intentiveness","intentness","interdict","intimation","ken","knowledge","lay eyes on","leader","leading article","lesson","levy","light","literary criticism","look","look on","look upon","looking","lookout","make out","mandamus","mandate","mandatory injunction","manifesto","mark","memo","mention","message","mind","mindfulness","mittimus","monition","moral","nisi prius","noesis","nonnegotiable demand","note","notification","notify","object lesson","observance","observation","observe","order","pay attention to","perceive","perception","pick out","pick up","position paper","precept","presentation","press release","process","proclamation","program","programma","prohibitory injunction","promotional material","pronouncement","pronunciamento","proof","public notice","publication","publicity","realization","recognition","recognize","refer","regard","regardfulness","release","remark","report","requirement","requisition","respect","review","running commentary","rush","rush order","search warrant","see","sense","sensibility","sidelight","sight","spot","spy","spying","statement","take heed of","take in","take note","take note of","take notice","take notice of","tax","taxing","tend","the dope","the goods","the know","the scoop","thought","threat","tip-off","transmission","tribute","twig","ukase","ultimatum","understanding","verbum sapienti","view","viewing","warn","warning","warning piece","warrant","warrant of arrest","warrant of attorney","watch","watching","white book","white paper","witness","witnessing","word","writ","write-up","notification","object lesson","observance","observation","observe","order","pamphlet","perceive","perception","pick out","pick up","pipe","position paper","poster","precept","presentation","process","proclamation","program","pronouncement","proof","publication","publicity","puff","push","realization","recognition","recognize","regard","release","remark","reminder","report","requirement","requisition","resignation","respect","review","reviewer","rush","savor","scoop","seal","search warrant","see","sense","sensibility","sidelight","sight","sign","spot","spy","spying","statement","take in","take notice","tax","taxing","tend","thought","threat","tip off","transmission","tribute","twig","ukase","ultimatum","view","warning","warrant","watch","white paper","witness","word to the wise","word","writ","write up"]
 syn "coat" = ["bedaub","bedizen","begild","besmear","blanket","boot","bristle","buff","butter","calcimine","cap","cloak","coat","coat","Eton jacket","Leatherette","Leatheroid","Mao jacket","anorak","apply paint","bedaub","bedizen","begild","besmear","blanket","blazer","blouse","body coat","bolero","bomber jacket","bonnet","boot","breech","bristle","brush on paint","butter","calcimine","cap","capillament","capuchin","car coat","chaqueta","chesterfield","chromogen","cilium","claw hammer","claw-hammer coat","cloak","coat of paint","coating","coif","collop","color","color filter","color gelatin","colorant","coloring","complexion","cover","coverage","covering","covert","coverture","cowl","cowling","curtain","cut","cutaway coat","cuticle","dab","daub","dead-color","deal","deep-dye","dermis","dinner jacket","dip","disk","distemper","double-dye","doublet","drape","drapery","dress coat","drier","duffel","dye","dyestuff","emblazon","enamel","engild","exterior paint","face","facing","fast-dye","fell","feuille","film","fingertip coat","fitted coat","flap","flat coat","flat wash","fleece","flesh","floor enamel","foil","fold","fresco","frock","frock coat","fur","furring","gild","glaze","gloss","gown","grain","greatcoat","ground","guise","hair","hanging","hat","hide","hood","horsehair","housing","hue","illuminate","imbue","imitation fur","imitation leather","ingrain","integument","interior paint","jacket","japan","jerkin","jumper","jupe","lacquer","lamella","lamina","laminated glass","laminated wood","lap","lay on","lay on color","layer","leaf","leather","leather paper","loden coat","mackinaw","mane","mantle","mask","medium","membrane","mess jacket","midicoat","monkey jacket","opaque color","outer layer","outer skin","overcoat","overlay","paint","pall","pane","panel","parget","parka","patina","pea jacket","peel","pellicle","pelt","peltry","pigment","pile","plait","plank","plate","plating","ply","plywood","prime","prime coat","primer","priming","pubescence","pubic hair","rasher","rawhide","reefer","revetment","rind","sack","safety glass","san benito","scale","screen","scum","setula","shade","shadow","shag","sheath","sheet","shellac","shelter","shield","shirt","shoe","shroud","ski jacket","skin","skins","slab","slap on","slat","slather","sleeve waistcoat","slice","slop on paint","smear","smear on","smoking jacket","sock","spiketail coat","spread","spread on","spread with","stain","stipple","stocking","swallowtail","tabard","table","tablet","tail coat","tails","tar","tegument","tempera","thinner","tinct","tinction","tincture","tinge","tint","tone","topcoat","transparent color","turpentine","turps","undercoat","undercoating","vair","varnish","vehicle","veil","veneer","vestment","wafer","wash","wash coat","watch coat","whitewash","windbreaker","wool","woolly","coating","coif","collop","color","coloring","cover","covering","covert","cowl","curtain","cut","cuticle","dab","daub","deal","dermis","dip","disk","distemper","drape","drapery","drier","dye","emblazon","emblem","enamel","face","facing","fell","film","flap","fleece","flesh","foil","fold","fresco","fur","gild","glaze","gloss","gown","grain","ground","guise","hair","hanging","hat","hide","hood","horsehair","housing","hue","illuminate","imbue","ingrain","integument","jacket","lacquer","lap","lay it on thick","lay on","leaf","leather","mane","mantle","mask","medium","membrane","overcoat","paint","pall","pane","panel","parget","patina","peel","pellicle","pelt","pigment","pile","plait","plank","plaster","plate","plating","ply","plywood","powder","prime","primer","priming","rasher","rawhide","rind","safety glass","scale","screen","scum","seta","shade","shadow","shag","sheath","sheet","shellac","shelter","shield","shoe","shroud","skin","slab","slap on","slat","slather","slice","smear","sock","spread","stain","stipple","table","tablet","tar","tempera","thinner","tincture","tinge","tint","tone","turpentine","undercoat","varnish","vehicle","veil","veneer","vestment","wafer","wash","whitewash","wool"] 
