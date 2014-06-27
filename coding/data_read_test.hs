cee30f493c3a9601a65bdfa30081f3677bf0c01a
Michael Skelly <skellystudios@gmail.com>
2014-06-27 14:29:40 +0100

    Text direct from the file ah yeah

--- coding/data_read_test.hs
+++ coding/data_read_test.hs
@@ -1,9 +1,29 @@
 import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
+import qualified Data.Map as Map 
 
-main = do
-    ls <- fmap Text.lines (Text.readFile "data.dat")
-    (print . words .head) ls
+import System.IO.Unsafe
 
+read_wordlist = do
+    ls <- fmap Text.lines (Text.readFile "straight-wordlist")
+    (return . string_read . toString . head) ls
+    
+wordlist = unsafePerformIO $ do read_wordlist
+
+read_thesaurus = do
+    ls <- fmap Text.lines (Text.readFile "thesaurus")
+    (return . Map.fromList . map_read . toString . head) ls
+    
+
+thesaurus = unsafePerformIO $ do read_thesaurus
+
+toString = Text.unpack
 
 -- do n <- readFile "data.dat" ; print n
+
+-- 
+map_read :: String -> [(String, [String])]
+map_read = read
+
+string_read :: String -> [String]
+string_read = read
