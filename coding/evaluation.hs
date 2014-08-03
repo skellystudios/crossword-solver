f598dcc8bf3e50ed995650a3b3d10f0776ebf775
Michael Skelly <skellystudios@gmail.com>
2014-08-03 17:53:30 +0100

    Deep into prefixes, but not too far off one valid point. Just about to fuck with eval_trees

--- coding/evaluation.hs
+++ coding/evaluation.hs
@@ -12,15 +12,17 @@ import Wordlists
 
 -- Now we evaluate
 eval :: Parse -> [Answer]
-eval (DefNode y z n) = let constraints = (n, n) in [Answer x (DefNode y z n) | x <- eval_tree n z] 
+eval (DefNode y z n) = let constraints = (n, n) in [Answer x (DefNode y z n) | x <- eval_tree z (Constraints (Prefix []) (Max n) (Min n))] 
 
 eval_tree :: ClueTree  -> EvalConstraints -> [String]
-eval_tree (AnagramNode x y) = let y' = (concat y) in 
-								if length y' > n then [] else  (delete y') . anagrams  $ y'
-eval_tree (Leaf x) (p mx mn) = filter (\x -> length x <= n) (syn x ++ [x])
-eval_tree (ConsListNode xs) (p mx mn) = eval_trees n xs (Prefix []) --map concat (sequence (map (eval_tree n) xs))
-{- eval_tree (ConsNode x y) = [x' ++ y' | x' <- eval_tree n x, y' <- eval_tree (n - length x') y]
-eval_tree (InsertionNode ind x y) = concat[insertInto x' y' | x' <- eval_tree n x, y' <- eval_tree n y, length y' == (n - length x')]
+eval_tree (AnagramNode x y) c = 
+								let y' = (concat y) in 	
+								if (is_less_than_min (minC c) length y') then [] else  filter (is_prefix_with (prefC c)) . (delete y') . anagrams  $ y'
+
+eval_tree (Leaf x) c = filter (fits c) (syn x ++ [x])
+eval_tree (ConsListNode xs) c = eval_trees (minC c) xs (Prefix []) --map concat (sequence (map (eval_tree n) xs))
+{- eval_tree (ConsNode x y) c = [x' ++ y' | x' <- eval_tree x (noPrefix c), y' <- eval_tree y (Constraints Unconstrained (mx - length x') (mn - length x'))]
+eval_tree (InsertionNode ind x y) (Constraints p mx mn) = concat[insertInto x' y' | x' <- eval_tree x, y' <- eval_tree mn y, length y' == (mn - length x')]
 eval_tree (SubtractionNode ind x y) = concat[subtractFrom x' y' | x' <- eval_tree 99 x, y' <- eval_tree 99 y, length y' - length x' == n  ]
 eval_tree (HiddenWordNode ind ys) = [x | x <- substr (concat ys), (length x) > 0, (length x) <= n, isInWordlist(x)]
 eval_tree (ReversalNode ind ys) = map reverse (eval_tree n ys)
@@ -30,12 +32,71 @@ eval_tree (PartialNode ind y) = concat [top_tail_substrings y | y <- eval_tree n
 eval_tree (ConsIndicatorLeaf x) = [""]
 -}
 
+
 eval_trees :: Int -> [ClueTree] -> PrefixConstraint -> [String]
-eval_trees n (c:[]) _ = eval_tree n c
-eval_trees n (c:clues_left) (Prefix pref) =
-  let starts = [start | start <- eval_tree n c] --, is_wordlist_prefix (pref ++ start)]
-  in concatMap f starts 
-  where f start =  map (\x -> start ++ x) (eval_trees (n - (length start)) clues_left (Prefix (pref ++ start)))
+eval_trees n (c:[]) _ = eval_tree c (Constraints Unconstrained (Max n) (Min n))
+eval_trees n (c:clues_left) p =
+  let starts = [start | start <- eval_tree c (Constraints Unconstrained (Max n) NoMin), is_prefix_with p start]
+  in filter is_prefix . concatMap f $ starts 
+  where f start =  map (\x -> start ++ x) (eval_trees (n - (length start)) clues_left (extend_prefix p start))
+
+
+--------- CONSTRAINTS
+
+class Constraint c where
+    fits :: c -> String -> Bool
+instance Constraint MaxLength where
+    fits mx s = is_lte_max mx (length s)
+instance Constraint PrefixConstraint where
+	fits p s = is_prefix_with p s
+
+
+is_prefix_with (Prefix p) x = is_prefix (p ++ x)
+is_prefix_with Unconstrained x = True
+
+is_lte_max (Max mx) n = n <= mx 
+is_lte_max NoMax n = True
+
+is_less_than_min (Min mn) n = n < mn 
+is_less_than_min NoMin n = True
+
+extend_prefix (Prefix p) x = Prefix (p ++ x)
+extend_prefix Unconstrained x = Unconstrained
+
+fits_Constraints (Constraints p mx mn) x = (fits p x) && (fits mx x) --x && fits mn x
+
+decreaseMax :: EvalConstraints -> Int -> EvalConstraints
+decreaseMax (Constraints p (Max mx) mn) n = Constraints p (Max (mx - n)) mn
+decreaseMax (Constraints p NoMax mn) n = Constraints p NoMax mn
+
+increaseMin :: EvalConstraints -> Int -> EvalConstraints
+increaseMin (Constraints p mx (Min mn)) n = Constraints p mx (Min (mn-n))
+increaseMin (Constraints p mx NoMin) n = Constraints p mx NoMin
+
+maxC :: EvalConstraints -> Int
+maxC (Constraints p (Max mx) (Min mn)) = mx
+
+minC :: EvalConstraints -> Int
+minC (Constraints p (Max mx) (Min mn)) = mn
+
+prefC :: EvalConstraints -> PrefixConstraint
+prefC (Constraints p mx mn) = p
+
+noPrefix :: EvalConstraints -> EvalConstraints
+noPrefix (Constraints p mx mn) = (Constraints Unconstrained mx mn)
+
+{-fits :: a -> String -> Bool
+fits (Max n) s = True
+fits (Prefix p) s = True
+-}
+
+{- instance Ord MinLength where
+	(<) (Min n) = (<) n
+	(<) NoMin = True
+	(>) NoMin = True
+	(>) (Min n) = (>) n
+	-}
+
 
 
 evaluate :: [Parse] -> [Answer]
@@ -89,6 +150,8 @@ top_substrings (x:xs) = [[x]] ++ (map  (\y -> [x] ++ y) (top_substrings xs))
 tail_substrings :: String -> [String]
 tail_substrings = (map reverse) . top_substrings . reverse
 
+{-
 check_eval :: Parse -> [Answer]
 -- check_eval x = let DefNode y z n = x in Data.List.intersect (syn y) ((eval_tree n z))
 check_eval (DefNode y z n) = map (\x -> Answer x (DefNode y z n)) (Data.Set.toList (Data.Set.intersection wordlist_extended (Data.Set.fromList (eval_tree n z))))
+-} 
