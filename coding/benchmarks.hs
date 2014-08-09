1a9fd0307b36ad6435156ca563f52fbd77a416f1
Michael Skelly <skellystudios@gmail.com>
2014-08-09 21:30:04 +0100

    Added cluebank to Naive

--- coding/benchmarks.hs
+++
@@ -1,5 +0,0 @@
-module Benchmarks (clues) where
-
-import Types
-
-clues = [Clue ("companion shredded corset",6), Clue ("notice in flying coat", 6), Clue ("companion found in oklahoma terminal", 4), Clue ("a new member returned a woman", 6), Clue ("pause at these i fancy", 8), Clue ("ankle was twisted in ballet", 8), Clue ("flyer needed by funfair manager", 6), Clue ("put food in this stuff on barge at sea", 9), Clue ("notice supervisor is going nuts at first", 4), Clue ("animal makes mistake crossing one river", 7), Clue ("maria not a fickle lover", 9), Clue ("hope for high praise", 6)]
